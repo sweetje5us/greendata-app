a6e7d262dac8a8fe726c96cf0414eda99114b816
sweetje5us <82095847+sweetje5us@users.noreply.github.com>
2021-04-16 22:47:33 +0500

    gdf
    fdg

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./index.css";
+
+export default function App() {
+  return (
+      <>
+    <buttongroup><img class="logo" src="https://greendatasoft.ru/wp-content/uploads/2018/05/лого-1.png" alt="greendata logo">
+    </img>
+    <button class="greenbutton buttonadd">
+    Add
+  </button>
+  <button class="greenbutton buttonedit">
+    Edit
+  </button>
+  <button class="redbutton buttondelete">
+    Delete
+  </button></buttongroup>
+  <table class="maintable">
+      <tableheader>
+          
+          <thbox>
+
+          </thbox>
+          <thunder>
+
+          </thunder>
+      </tableheader>
+      <tablecontent>
+          <tableitem>
+
+          </tableitem>
+          <tableitem>
+              
+          </tableitem>
+          <tableitem>
+              
+          </tableitem>
+          <tableitem>
+              
+          </tableitem>
+          <tableitem>
+              
+          </tableitem>
+          <tableitem>
+              
+          </tableitem>
+          <tableitem>
+              
+          </tableitem>
+      </tablecontent>
+  </table>
+  <footer>
+      <copyrights>
+      © GreenData|2014-2021
+      </copyrights>
+      <author>
+      Made by Brylove Eugene
+      </author>
+  </footer>
+     </>
+  
+    
+  );
+}
